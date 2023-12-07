9e7e9e696f4bb61da9077c43cb23e668c23ee1d1
KarenB <karenabourgeois@gmail.com>
2023-12-07 11:52:27 -0400

    Tring to figure out clicks

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,33 +1,33 @@
 var search = document.getElementById('search');
 var mainSubmit = document.querySelector('main-submit');
+var city = document.querySelector('city')
 var date = document.querySelector('date');
 var humidity = document.querySelector('humidity');
 var wind = document.querySelector('wind');
 var temp = document.querySelector('termperature');
-var searchInp = '';
+
 
 var weatherAPIKey = '4edbc2e4e408c4f8b63a6eb2e13be206';
 var weatherEndPoint = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=' + weatherAPIKey;
 
 
-// searchInp.addEventListener('keydown', async (e) => {
+// search.addEventListener('keydown', async (e) => {
 //     if(e.keyCode === 13) {
-//         let currentWeather = await byCityNameWeather(searchInp.value);
-//         console.log(currentWeather);
+//         let currentWeather = await byCityNameWeather(search.value);
+//         console.log(e);
 //     }
-// })
+// });
 
-searchInp.addEventListener('click', async (e) => {
-    if(e) {
-        let currentWeather = await byCityNameWeather(searchInp.value);
-        console.log(currentWeather);
-    }
-})
+mainSubmit.addEventListener('click', function() {
+    alert('hello');
+});
 
 var byCityNameWeather = async (city) => {
     let end = weatherEndPoint + '&q=' + city;
     let response = await fetch (end);
     let currentWeather = await response.json();
     return currentWeather;
+    
 }
 
+byCityNameWeather();
